9b12b86f5755ac9253bd686f2ffffddb0b3e5191
Lucas Heidy <nlucashtg@gmail.com>
2021-05-10 21:39:35 -0300

    questScreen instável, n dar merge

--- app base/lib/services/database.dart
+++ app base/lib/services/database.dart
@@ -51,6 +51,19 @@ class DatabaseMethods {
     });
   }
 
+  addRespostaPromisn1(String emailId, dominioMap) {
+    FirebaseFirestore.instance
+        .collection("Quests")
+        .doc(emailId)
+        .collection("userQuests")
+        .doc("promisn1")
+        .collection("userPromisn1")
+        .add(dominioMap)
+        .catchError((e) {
+      print(e.toString());
+    });
+  }
+
   getConversationMessages(String chatRoomId) async {
     return FirebaseFirestore.instance
         .collection("ChatRoom")
@@ -58,6 +71,25 @@ class DatabaseMethods {
         .collection("chats")
         .orderBy("time", descending: false)
         .snapshots();
+  }
+
+  getValuePromisn1(String userEmail) async {
+    return FirebaseFirestore.instance
+        .collection("Quests")
+        .doc(userEmail)
+        .collection("userQuests")
+        .doc("promisn1")
+        .collection("userPromisn1")
+        .limitToLast(1)
+        .get();
+  }
+
+  getQuests(String userEmail) async {
+    return FirebaseFirestore.instance
+        .collection('Quests')
+        .doc(userEmail)
+        .collection("userQuests")
+        .get();
   }
 
   getChatRooms(String userName) async {
--- app base/lib/views/quests_screen.dart
+++ app base/lib/views/quests_screen.dart
@@ -1,11 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../escalas/promisn1/promisn1_screen.dart';
 //import './promisn2_screen.dart';
+import 'package:chat_app_tutorial/helper/helperfuncions.dart';
+import 'package:chat_app_tutorial/services/database.dart';
 
-class QuestsScreen extends StatelessWidget {
+class QuestsScreen extends StatefulWidget {
   static const routeName = '/quests-screen';
+  @override
+  _QuestsScreenState createState() => _QuestsScreenState();
+}
+
+class _QuestsScreenState extends State<QuestsScreen> {
+  DateTime pickedDate;
+  DatabaseMethods databaseMethods = new DatabaseMethods();
+  String userEmail;
+  Stream questsScreenStream;
+
+  @override
+  void initState() {
+    getUserInfo();
+    super.initState();
+    pickedDate = DateTime.now();
+  }
+
+  getUserInfo() async {
+    userEmail = await HelperFunctions.getUserEmailInSharedPreference();
+    databaseMethods.getQuests(userEmail).then((val) {
+      setState(() {
+        questsScreenStream = val;
+      });
+    });
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,28 +43,48 @@ class QuestsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Questionários'),
       ),
-      body: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              ListTile(
-                title: Text('PROMIS Nível 1'),
-                onTap: () {
-                  Navigator.of(context).pushNamed(Promisn1Screen.routeName,
-                      arguments: {'title': 'PROMIS Nível 1'});
-                },
-              ),
-              Divider(thickness: 2.0),
-              /*ListTile(
-                title: Text('PROMIS Nível 2'),
-                onTap: () {
-                  Navigator.of(context).pushNamed(Promisn2Screen.routeName,
-                      arguments: {'title': 'PROMIS Nível 2'});
-                },
-              ),*/
-              Divider(thickness: 2.0),
-            ],
-          )),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: questsScreenStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    print(snapshot.data.docs[index].get("questName"));
+                    print(index);
+                    return QuestsTile(
+                        snapshot.data.docs[index].get("questName"),
+                        snapshot.data.docs[index]);
+                  },
+                )
+              : Container();
+        },
+      ),
     );
+  }
+}
+
+class QuestsTile extends StatelessWidget {
+  final String questName;
+  final DocumentSnapshot snap;
+
+  QuestsTile(this.questName, this.snap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(questName),
+              onTap: () {
+                Navigator.of(context).pushNamed(Promisn1Screen.routeName,
+                    arguments: {'title': 'PROMIS Nível 1'});
+              },
+            ),
+            Divider(thickness: 2.0),
+          ],
+        ));
   }
 }
