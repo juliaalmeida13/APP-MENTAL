306f699c3cfd973e3888c1d62d4ca2134497609b
Isabella Costa <isabellacostap@gmail.com>
2021-06-25 12:12:36 -0300

    Otimizar tela de intervenção para responsividade

---
+++ app base/lib/Screens/Reading/Widgets/img_and_text_info.dart
@@ -0,0 +1,170 @@
+import 'package:chat_app_tutorial/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class ImgAndTextInfoCard extends StatelessWidget {
+  const ImgAndTextInfoCard({
+    Key key,
+    this.interventionImage,
+    @required this.size, this.interventionText,
+  }) : super(key: key);
+
+  final Size size;
+  final String interventionImage;
+  final String interventionText;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment:Alignment(0,-1),
+      child:SingleChildScrollView(
+        child:Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ImageCard(size: size, image: interventionImage),
+            InfoTextCard(size: size, textFile: interventionText)
+          ],
+        ),
+      ),
+    );
+  }
+}
+///imagem ilustrativa
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    Key key,
+    this.image,
+    @required this.size,
+  }) : super(key: key);
+
+  final Size size;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width * 0.9,
+      height: 200,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment(0, 1),
+            child: Container(
+              width: size.width,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color(0xFFEEEEEE),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image(
+                image: AssetImage(image),
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 250,
+                fit: BoxFit.cover
+              )
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
+///Texto da intervencao
+class InfoTextCard extends StatelessWidget {
+  const InfoTextCard({
+    Key key,
+    @required this.size, this.textFile,
+  }) : super(key: key);
+
+  final Size size;
+  final String textFile;
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment(0, 1),
+      child: Container(
+        width: size.width * 0.9,
+        constraints: BoxConstraints(
+          maxHeight: double.infinity,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xFFEEEEEE),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(0),
+          ),
+          shape: BoxShape.rectangle,
+        ),
+        alignment: Alignment(-1, -1),
+        child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: InfoText(markdownFile: textFile )
+        ),
+      ),
+    );
+  }
+}
+/// Markdown Text Widget
+class InfoText extends StatelessWidget {
+  const InfoText({
+    Key key,
+    @required this.markdownFile,
+  }) : super(key: key);
+
+  final String markdownFile;
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return FutureBuilder(
+        future: rootBundle.loadString(markdownFile),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Container(
+              margin: EdgeInsets.only(bottom: kDefaultPadding),
+              child: SizedBox(
+                height: size.height*0.53,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Markdown(
+                          styleSheet: MrkdnTextTheme(context),
+                          data: snapshot.data),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+
+  /// Markdown Text Style
+  MarkdownStyleSheet MrkdnTextTheme(BuildContext context) {
+    return MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+        textScaleFactor: 1.5,
+        p: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.66)));
+  }
+}
--- app base/lib/Screens/Reading/reading_screen.dart
+++ app base/lib/Screens/Reading/reading_screen.dart
@@ -1,15 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:chat_app_tutorial/Screens/Reading/Widgets/body.dart';
+import 'package:flutter/material.dart';
+
+import 'package:chat_app_tutorial/constants.dart';
 
 class ReadingScreen extends StatefulWidget {
   @override
   _ReadingScreenState createState() => _ReadingScreenState();
 }
 
+
 class _ReadingScreenState extends State<ReadingScreen> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+        appBar: AppBar(
+          backgroundColor: kPrimaryGreen,
+          automaticallyImplyLeading: true,
+          actions: [],
+          centerTitle: true,
+          elevation: 4,
+        ),
       body: Body(),
     );
   }
