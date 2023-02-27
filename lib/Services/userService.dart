b8140412118fb6f7fceb26f37bffbaa2b02d19af
LuisRotoly <lu.gustavo.lima1@gmail.com>
2023-02-27 19:51:35 -0300

    Retiradas funções do userService

--- lib/Services/userService.dart
+++ lib/Services/userService.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:app_mental/model/exceptions/HttpException.dart';
 import 'package:app_mental/model/exceptions/apiError.dart';
@@ -24,45 +25,6 @@ class UserService {
         }));
     if (response.statusCode == 200) {
       return UserApp.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
-    }
-    final error =
-        ApiError.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
-    throw HttpException(error.message.toString());
-  }
-
-  addNewSleepDiary(String userEmail, Map answerQuestions) async {
-    final response =
-        await http.post(Uri.parse("${url}addAnswerSleepQuestionnaire"),
-            headers: <String, String>{
-              'Content-Type': 'application/json; charset=UTF-8',
-            },
-            body: jsonEncode(<String, String>{
-              'userEmail': userEmail,
-              'gotoBed': answerQuestions['resp1'],
-              'tryToSleep': answerQuestions['resp2'],
-              'whileToSleep': answerQuestions['resp3'],
-              'timesWokeUp': answerQuestions['resp4'],
-              'timeAwake': answerQuestions['resp5'],
-              'wakeUpTime': answerQuestions['resp6'],
-              'spendInBed': answerQuestions['resp7'],
-              'sleepDuringDay': answerQuestions['resp8'],
-            }));
-    if (response.statusCode != 200) {
-      final error =
-          ApiError.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
-      throw HttpException(error.message.toString());
-    }
-  }
-
-  Future<bool> isSleepDiaryAnsweredToday(String userEmail) async {
-    final response = await http.get(
-      Uri.parse("${url}isAnsweredSleepQuestionnaireToday?email=$userEmail"),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    );
-    if (response.statusCode == 200) {
-      return jsonDecode(utf8.decode(response.bodyBytes));
     }
     final error =
         ApiError.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
