fbc6f7a597a63fcfbb50c105df3a60e968e3fb67
LuisRotoly <lu.gustavo.lima1@gmail.com>
2023-06-27 13:28:11 -0300

    Alterado icone das leituras e retorno do id da answer_options

--- lib/model/answers.dart
+++ lib/model/answers.dart
@@ -1,17 +1,20 @@
 class Answers {
+  final int answerId;
   final String answerText;
   final String score;
   final int domain;
   final int order;
 
   const Answers(
-      {required this.answerText,
+      {required this.answerId,
+      required this.answerText,
       required this.score,
       required this.domain,
       required this.order});
 
   factory Answers.fromJson(Map<String, dynamic> json) {
     return Answers(
+      answerId: json['answerId'],
       answerText: json['answerText'],
       score: json['score'],
       domain: json['domain'],
