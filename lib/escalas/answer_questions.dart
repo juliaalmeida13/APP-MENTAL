6b9d11d65d7c4ff0bc8f43b59c2ebc193c62fe30
LuisRotoly <lu.gustavo.lima1@gmail.com>
2023-06-29 10:03:57 -0300

    Alterado para receber o copsoq

--- lib/escalas/answer_questions.dart
+++ lib/escalas/answer_questions.dart
@@ -1,13 +1,13 @@
 import 'package:app_mental/escalas/promis_answer.dart';
-import 'package:app_mental/escalas/promis_answer_input.dart';
 import 'package:app_mental/escalas/question.dart';
 import 'package:app_mental/helper/constants.dart';
 import 'package:app_mental/model/answers.dart';
 import 'package:flutter/material.dart';
 
 import '../Services/questionnaireService.dart';
+import '../model/questionnaire_answer.dart';
 
-class AnswerQuestions extends StatelessWidget {
+class AnswerQuestions extends StatefulWidget {
   final int sizeQuestionnaire;
   final String question;
   final List<Answers> answers;
@@ -18,6 +18,7 @@ class AnswerQuestions extends StatelessWidget {
   final String scale;
   final String questionnaireCode;
   final String questName;
+  final Function setQuestionIndex;
 
   AnswerQuestions(
       {required this.answers,
@@ -29,20 +30,35 @@ class AnswerQuestions extends StatelessWidget {
       required this.userEmail,
       required this.scale,
       required this.questionnaireCode,
-      required this.questName});
+      required this.questName,
+      required this.setQuestionIndex});
+
+  @override
+  State<AnswerQuestions> createState() => _AnswerQuestionsState();
+}
+
+class _AnswerQuestionsState extends State<AnswerQuestions> {
+  List<bool> checkboxValueList = [false, false, false, false];
+  final textController = TextEditingController();
+
+  changeCheckboxValue(newValue, index) {
+    setState(() {
+      checkboxValueList[index] = newValue!;
+    });
+  }
 
   String getQuestionText() {
-    if (questionnaireCode == QuestionnaireCode.assistn2.name) {
-      final startSubstance = questName.split("(");
+    if (widget.questionnaireCode == QuestionnaireCode.assistn2.name) {
+      final startSubstance = widget.questName.split("(");
       final substance = startSubstance[1].split(")");
-      final questionText = question + " " + substance[0];
+      final questionText = widget.question + " " + substance[0];
       return questionText;
     } else {
-      if (questionnaireCode == QuestionnaireCode.pset.name &&
-          scale == "pset_week2") {
-        return "Durante sua vida " + question;
+      if (widget.questionnaireCode == QuestionnaireCode.pset.name &&
+          widget.scale == "pset_week2") {
+        return "Durante sua vida " + widget.question;
       } else {
-        return "No último mês " + question;
+        return "No último mês " + widget.question;
       }
     }
   }
@@ -54,7 +70,8 @@ class AnswerQuestions extends StatelessWidget {
 
   discartChanges(BuildContext context) async {
     QuestionnaireService()
-        .discardAllAnswers(userEmail, QuestionnaireCode.ccsm.name, scale)
+        .discardAllAnswers(
+            widget.userEmail, QuestionnaireCode.ccsm.name, widget.scale)
         .then((_) {
       Navigator.of(context).popUntil(ModalRoute.withName('/logged-home'));
       Navigator.of(context).pushNamed("/quests-screen");
@@ -74,7 +91,7 @@ class AnswerQuestions extends StatelessWidget {
               onPressed: () => Navigator.pop(context, 'Cancel'),
               child: const Text('Cancelar'),
             ),
-            (questionnaireCode == QuestionnaireCode.ccsm.name)
+            (widget.questionnaireCode == QuestionnaireCode.ccsm.name)
                 ? TextButton(
                     onPressed: () => discartChanges(context),
                     child: const Text('Descartar'),
@@ -90,6 +107,141 @@ class AnswerQuestions extends StatelessWidget {
     );
   }
 
+  sendAllChosenCheckboxes(index) {
+    for (int i = 0; i < checkboxValueList.length; i++) {
+      if (checkboxValueList[i]) {
+        QuestionnaireAnswer questionnaireAnswer = new QuestionnaireAnswer(
+            answerId: widget.answers[i].answerId,
+            email: widget.userEmail,
+            answer: widget.answers[i].answerText,
+            score: widget.answers[i].score,
+            domain: widget.answers[i].domain,
+            code: widget.questionnaireCode,
+            questionIndex: widget.questionIndex,
+            scale: widget.scale);
+        QuestionnaireService().addQuestionnaireAnswer(questionnaireAnswer);
+        setState(() {
+          checkboxValueList[i] = false;
+        });
+      }
+    }
+    QuestionnaireAnswer questionnaireAnswer = new QuestionnaireAnswer(
+        answerId: widget.answers[index].answerId,
+        email: widget.userEmail,
+        answer: widget.answers[index].answerText,
+        score: widget.answers[index].score,
+        domain: widget.answers[index].domain,
+        code: widget.questionnaireCode,
+        questionIndex: widget.questionIndex,
+        scale: widget.scale);
+    QuestionnaireService().addQuestionnaireAnswer(questionnaireAnswer);
+    widget.setQuestionIndex(widget.questionIndex + 1);
+  }
+
+  sendText() {
+    if (textController.text == "") {
+      textController.text = "Continuar";
+    }
+    QuestionnaireAnswer questionnaireAnswer = new QuestionnaireAnswer(
+        answerId: widget.answers[0].answerId,
+        email: widget.userEmail,
+        answer: textController.text,
+        score: textController.text,
+        domain: widget.answers[0].domain,
+        code: widget.questionnaireCode,
+        questionIndex: widget.questionIndex,
+        scale: widget.scale);
+    QuestionnaireService().addQuestionnaireAnswer(questionnaireAnswer);
+    widget.setQuestionIndex(widget.questionIndex + 1);
+  }
+
+  List<Widget> getListAnswers() {
+    if (widget.scale == "copsoq_week2" && widget.questionIndex == 45) {
+      return [
+        Column(children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextFormField(
+              maxLines: 4,
+              controller: textController,
+              maxLength: 100,
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton(
+              onPressed: sendText,
+              child: Text(
+                "Continuar",
+                textAlign: TextAlign.center,
+              ),
+              style: new ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.all(12))),
+            ),
+          ),
+        ]),
+      ];
+    } else if ((widget.scale == "copsoq_week2" &&
+        (widget.questionIndex == 38 ||
+            widget.questionIndex == 40 ||
+            widget.questionIndex == 42 ||
+            widget.questionIndex == 44))) {
+      return [
+        ...(widget.answers.map((answer) {
+          int index = widget.answers.indexOf(answer);
+          if (index == 4 || index == 5) {
+            return OutlinedButton(
+              onPressed: () => sendAllChosenCheckboxes(index),
+              child: Text(
+                answer.answerText,
+                textAlign: TextAlign.center,
+              ),
+              style: new ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      EdgeInsets.all(12))),
+            );
+          }
+          return AnswerOption(
+            () => widget.answerQuestion(
+                answer.answerId,
+                answer.score,
+                answer.domain,
+                answer.answerText,
+                widget.scale,
+                widget.questionnaireCode),
+            answer.answerText,
+            widget.scale,
+            widget.questionIndex,
+            checkboxValueList[index],
+            changeCheckboxValue,
+            index,
+          );
+        })).toList(),
+      ];
+    } else {
+      return [
+        ...(widget.answers.map((answer) {
+          return AnswerOption(
+            () => widget.answerQuestion(
+                answer.answerId,
+                answer.score,
+                answer.domain,
+                answer.answerText,
+                widget.scale,
+                widget.questionnaireCode),
+            answer.answerText,
+            widget.scale,
+            widget.questionIndex,
+            checkboxValueList[0],
+            changeCheckboxValue,
+            0,
+          );
+        })).toList(),
+      ];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -99,109 +251,99 @@ class AnswerQuestions extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         color: Color.fromRGBO(244, 244, 244, 1),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * .60,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    child: Column(
-                      children: [
-                        Text(
-                          "Questão $questionIndex de $sizeQuestionnaire",
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(height: 10),
-                        (questionnaireCode == QuestionnaireCode.assistn2.name ||
-                                questionnaireCode ==
-                                    QuestionnaireCode.pset.name)
-                            ? Question(getQuestionText())
-                            : Question(question),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: SingleChildScrollView(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * .60,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          ...(answers.map((answer) {
-                            return AnswerOption(
-                              () => answerQuestion(
-                                  answer.answerId,
-                                  answer.score,
-                                  answer.domain,
-                                  answer.answerText,
-                                  scale,
-                                  questionnaireCode),
-                              answer.answerText,
-                            );
-                          })).toList()
+                          Text(
+                            "Questão ${widget.questionIndex} de ${widget.sizeQuestionnaire}",
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          (widget.questionnaireCode ==
+                                      QuestionnaireCode.assistn2.name ||
+                                  widget.questionnaireCode ==
+                                      QuestionnaireCode.pset.name)
+                              ? Question(getQuestionText())
+                              : Question(widget.question),
                         ],
                       ),
                     ),
+                    Container(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: getListAnswers(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(104, 202, 138, 1)),
+                    onPressed: () {
+                      widget.resetQuestion();
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/icons/questionarios_responder-depois.png",
+                          width: 40,
+                          height: 40,
+                        ),
+                        const Text(
+                          "Voltar para a questão anterior",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(104, 202, 138, 1)),
+                    onPressed: () {
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => answerLater(context),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/icons/questionarios_responder-agora.png",
+                          width: 40,
+                          height: 40,
+                        ),
+                        const Text('Responder depois',
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
-          Container(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(104, 202, 138, 1)),
-                  onPressed: () {
-                    resetQuestion();
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/icons/questionarios_responder-depois.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                      const Text(
-                        "Voltar para a questão anterior",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(104, 202, 138, 1)),
-                  onPressed: () {
-                    showDialog<String>(
-                      context: context,
-                      builder: (BuildContext context) => answerLater(context),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/icons/questionarios_responder-agora.png",
-                        width: 40,
-                        height: 40,
-                      ),
-                      const Text('Responder depois',
-                          style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
